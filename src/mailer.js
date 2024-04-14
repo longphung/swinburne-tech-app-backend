4cee671e171563f037447e0ec6de44c8a1678f97
Phung Nguyen <longphung.nn@gmail.com>
2024-04-14 14:24:25 +1000

    ✨ feat: install nodemailer

---
+++ src/mailer.js
@@ -0,0 +1,13 @@
+import nodemailer from "nodemailer";
+
+const mailer = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
+  secure: process.env.SMTP_SECURE === "1", // Use `true` for port 465, `false` for all other ports
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  },
+});
+
+export default mailer;
