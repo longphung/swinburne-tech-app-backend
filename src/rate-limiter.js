a249e2fd50387f72e48474555f6fd4dc7f5affed
Phung Nguyen <longphung.nn@gmail.com>
2024-05-01 12:09:00 +1000

    ✨ feat: update rate limiter for proxy

--- src/rate-limiter.js
+++ src/rate-limiter.js
@@ -4,6 +4,7 @@ const rateLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 1000, // limit each IP to 1000 requests per windowMs
   message: "Too many requests from this IP, please try again after 15 minutes",
+  keyGenerator: req => req.headers['cf-connecting-ip'] || req.ip,
 });
 
 export default rateLimiter;
